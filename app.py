f1507615995af7fc5be0a059824f0a39782eada0
genek771 <genek771@yandex.ru>
2019-04-08 15:00:39 +0300

    добавили раздел "блог", сделали поиск по тэгам

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from config import Configuration
+from flask_sqlalchemy import SQLAlchemy
 
-from posts.blueprint import posts
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
-app.register_blueprint(posts, url_prefix=('/blog'))
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 class Configuration(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://ms_admin:ms_admin@localhost/molchanov_flask'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
